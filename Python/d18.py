278f8c0058fdb9b728469bf86fb58ad3ff06eff5
shobhit7390 <vms6402@gmail.com>
2023-02-16 13:12:15 +0530

    While loop

---
+++ Python/d18.py
@@ -0,0 +1,19 @@
+# while loop
+
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am inside else")
+
+
+
+i=int(input("Enter the number: "))
+
+while(i<=38):
+    i=int(input("Enter the number: "))
+    print(i)
+print("Done with the loop")
+
