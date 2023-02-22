34220d6681d1dcb7a27b551df6cb5ade30998b81
shobhit7390 <vms6402@gmail.com>
2023-02-22 11:51:23 +0530

    if __name__==__main__

---
+++ Python/Shobhit.py
@@ -0,0 +1,10 @@
+# This  is a file which will be imported
+# __name=="__main__"
+
+def welcome():
+    print("Hey you are welcome from Shobhit")
+
+print(__name__)
+
+if __name__=="__main__":
+    welcome()
---
+++ Python/d45.py
@@ -0,0 +1,11 @@
+# importing Shobhit.py
+
+import Shobhit
+
+Shobhit.welcome()   
+
+            # The above code prints the statmnt 2 time , firstly from import line and secondly when it is called
+            # To get rid of this __name__=="__main__" is used
+
+
+
